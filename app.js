7e514ab40e883324a9fe1a2917cd5cf2505532de
wilker <wilker.vivas.31@gmail.com>
2025-12-06 06:59:09 -0400

    add cors origin localhost

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ import expresss from "express";
 const app = expresss();
 import { routerTransaction } from "./routes/transaction.js";
 import { routerStats } from "./routes/stats.js";
+import cors from 'cors'
 
 app.use(expresss.json());
 app.disable('x-powered-by')
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 
 app.use("/api/transactions", routerTransaction);
 app.use('/api/stats', routerStats)
