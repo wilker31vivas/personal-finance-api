ecfc6a2487e8f8c84d2b0db5c87bde5c61cf5e69
wilker <wilker.vivas.31@gmail.com>
2026-02-13 10:26:19 -0400

    update gitignore

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-import expresss from "express";
-const app = expresss();
+import "dotenv/config";
+import express from "express";
+const app = express();
 import { routerTransaction } from "./routes/transaction.js";
 import { routerStats } from "./routes/stats.js";
 import cors from "cors";
 import { categoriesRouter } from "./routes/categories.js";
 
-app.use(expresss.json());
-app.disable('x-powered-by')
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+app.use(express.json());
+app.disable("x-powered-by");
 
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN.includes(",") 
+      ? process.env.CORS_ORIGIN.split(",")
+      : process.env.CORS_ORIGIN,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
 
 app.use("/api/transactions", routerTransaction);
 app.use("/api/stats", routerStats);
@@ -23,5 +26,5 @@ app.use("/api/categories", categoriesRouter);
 const PORT = process.env.PORT ?? 3000;
 
 app.listen(PORT, () => {
-  console.log("Servidor ejecutandose", PORT);
+   console.log(`Servidor ejecutándose en puerto ${PORT}`);
 });
